4a4fb8fc7596a73d152ed379df8561ada16e39e2
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-03-13 13:06:38 +0100

    feat: I add parametrizedTest to add method two numbers, I test positive and negative numbers.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,25 @@
+
+import org.example.calculator.Calculator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
 
+    Calculator calculadora = new Calculator();
 
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,3,5",
+            "-2,-3,-5",
+            "3,-2,1"
+
+    })
+    public void addTwoNumbers(int number1,int number2, int result){
+        assertEquals(result, calculadora.add(number1,number2));
+    }
 
 }
