59c66a76629672f56b54e74448703ee6c3096880
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-03-13 13:07:28 +0100

    feat: I add parametrizedTest to substract method two numbers, I test positive and negative numbers.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -22,4 +22,15 @@ public class CalculatorTest {
         assertEquals(result, calculadora.add(number1,number2));
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,0",
+            "2,3,-1",
+            "-2,-3,1",
+            "3,-2,5",
+            "-3,2,-5"
+    })
+    public void substractTwoNumbers(int number1,int number2, int result){
+        assertEquals(result, calculadora.substract(number1,number2));
+    }
 }
